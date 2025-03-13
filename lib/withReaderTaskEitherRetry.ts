8d7941433b58a3bda2fc53d2b4c4f9fcdd6c715d
Nikos Tsompanidis <nikostsompanides@gmail.com>
2025-03-13 14:51:15 +0200

    chore: copy p-retry license

--- lib/withReaderTaskEitherRetry.ts
+++ lib/withReaderTaskEitherRetry.ts
@@ -1,16 +1,11 @@
 import { withTaskEitherRetry } from './withTaskEitherRetry.ts';
 import type * as ReaderTaskEither from 'fp-ts/lib/ReaderTaskEither.js';
-import type { RetriesExceededError } from './errors.ts';
 import type { Options } from 'p-retry';
 
 export const withReaderTaskEitherRetry =
   (options?: Options) =>
   <DependencyType, LeftType, RightType>(
     rte: ReaderTaskEither.ReaderTaskEither<DependencyType, LeftType, RightType>
-  ): ReaderTaskEither.ReaderTaskEither<
-    DependencyType,
-    RetriesExceededError,
-    RightType
-  > =>
+  ): ReaderTaskEither.ReaderTaskEither<DependencyType, LeftType, RightType> =>
   (env: DependencyType) =>
     withTaskEitherRetry(options)(rte(env));
